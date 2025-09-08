8272dbd396c6b841ff4ca3ab085bdf7b6182f7f2
estemateus <estemateusmkt@gmail.com>
2025-09-08 13:30:45 -0300

    Initial commit

---
+++ projetos_pessoais/mariaclara.py
@@ -0,0 +1,9 @@
+name = input("Quem fala comigo? ").lower().strip()
+if name == "maria" or name == "maria clara" or name == "marie":
+    print("VOCÊ FOI ETERNAMENTE AMALDIÇOADA, " + name + "!" + " SUA ALMA É MINHA AGORA!" + " HAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAAHHAHAAHAHAHAHAHAHAHAHAH!")
+if name != "maria" and name != "maria clara" and name != "marie":
+    print("POIS A MINHA ALMA INFERNAL PROCURA POR UMA MARIA CLARA ESTEVES BRAGA!" + " VOCÊ É ELA?")
+resposta = input("DIGA SIM OU NÃO! ").lower().strip()
+if resposta == "sim":
+    input("A morte está vindo atrás de você... Se prepare, " + name + "!")
+input("Pressione Enter para sair...")
