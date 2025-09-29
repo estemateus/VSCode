a6f34e502445c03289eb285278b01749d45a3fbd
estemateus <estemateusmkt@gmail.com>
2025-09-29 10:10:20 -0300

    att

---
+++ Faculdade/Unidade3/Aula1/aula1_exercicio.py
@@ -0,0 +1,43 @@
+# EXERCÍCIO
+# CRIAR UMA TABELA DE CONTATOS QUE QUE ARMAZENE INFORMAÇÕES COMO NOME, E-MAIL E NÚMERO DE TELEFONE
+
+import sqlite3
+
+# CREATE (Criação da tabela e inserção de dados de exemplo)
+conn = sqlite3.connect('contatos.db')
+cursor= conn.cursor()
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Contatos(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nome TEXT,
+        email TEXT,
+        telefone TEXT
+   )
+''')
+
+dados_exemplo = [
+    ('João', 'joao@email.com', '123-456-7890'),
+    ('Maria', 'maria@email.com', '987-654-3210'),
+    ('Carlos', 'carlos@email.com', '555-555-5555')
+]
+
+cursor.executemany('INSERT INTO Contatos (nome, email, telefone) VALUES (?, ?, ?)', dados_exemplo)
+
+conn.commit()
+
+# READ (Leitura e exibição dos contatos)
+cursor.execute('SELECT * FROM Contatos')
+contatos = cursor.fetchall()
+print("Contatos:")
+for contato in contatos:
+    print(contato)
+
+# UPDATE (Atualização do número de telefone do contato com ID 2)
+novo_telefone = '999-999-9999'
+contato_id = 2
+
+cursor.execute('UPDATE Contatos SET telefone  = ? WHERE id = ?', (novo_telefone, contato_id))
+conn.commit()
+
+# Fechando a conexão
+conn.close()
