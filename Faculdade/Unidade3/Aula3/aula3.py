be6ad3ce7711067a7dfa2bd8d7a7d606026d6428
estemateus <estemateusmkt@gmail.com>
2025-09-30 09:11:02 -0300

    att

---
+++ Faculdade/Unidade3/Aula3/aula3.py
@@ -0,0 +1,110 @@
+# INTRODUÇÃO A MANIPULAÇÃO DE DADOS EM PANDAS
+
+
+
+'''
+Descrição do Dado: CSV
+Tipos de Dado: Texto
+Método para Leitura: read_csv
+Método para Escrita: to_csv
+
+Descrição do Dado: Pipe-width Text
+Tipos de Dado: Texto
+Método para Leitura: read_table
+Método para Escrita: to_table
+
+Descrição do Dado: JSON
+Tipos de Dado: Texto
+Método para Leitura: read_json
+Método para Escrita: to_json
+
+Descrição do Dado: HTML
+Tipos de Dado: Texto
+Método para Leitura: read_html
+Método para Escrita: to_html
+
+Descrição do Dado: LaTeX
+Tipos de Dado: Texto
+Método para Leitura: read_latex
+Método para Escrita: styler.to_latex
+
+Descrição do Dado: XML
+Tipos de Dado: Texto
+Método para Leitura: read_xml
+Método para Escrita: to_xml
+
+Descrição do Dado: Local Clipboard
+Tipos de Dado: Texto
+Método para Leitura: read_clipboard
+Método para Escrita: to_clipboard
+
+Descrição do Dado: Excel
+Tipos de Dado: Binário
+Método para Leitura: read_excel
+Método para Escrita: to_excel
+
+Descrição do Dado: MS Access
+Tipos de Dado: Binário
+Método para Leitura: read_access
+Método para Escrita: to_access
+
+Descrição do Dado: OpenDocument
+Tipos de Dado: Binário
+Método para Leitura: read_ods
+Método para Escrita: to_ods
+
+Descrição do Dado: HDF5 Format
+Tipos de Dado: Binário
+Método para Leitura: read_hdf
+Método para Escrita: to_hdf
+
+Descrição do Dado: Feather Format
+Tipos de Dado: Binário
+Método para Leitura: read_feather
+Método para Escrita: to_feather
+
+Descrição do Dado: Parquet Format
+Tipos de Dado: Binário
+Método para Leitura: read_parquet
+Método para Escrita: to_parquet
+
+Descrição do Dado: ORC Format
+Tipos de Dado: Binário
+Método para Leitura: read_orc
+Método para Escrita: to_orc
+
+Descrição do Dado: MsgPack
+Tipos de Dado: Binário
+Método para Leitura: read_msgpack
+Método para Escrita: to_msgpack
+
+Descrição do Dado: Stata
+Tipos de Dado: Binário
+Método para Leitura: read_stata
+Método para Escrita: to_stata
+
+Descrição do Dado: SAS
+Tipos de Dado: Binário
+Método para Leitura: read_sas
+Método para Escrita: to_sas
+
+Descrição do Dado: SPSS
+Tipos de Dado: Binário
+Método para Leitura: read_spss
+Método para Escrita: to_spss
+
+Descrição do Dado: Pickle
+Tipos de Dado: Binário
+Método para Leitura: read_pickle
+Método para Escrita: to_pickle
+
+Descrição do Dado: SQL
+Tipos de Dado: SQL
+Método para Leitura: read_sql
+Método para Escrita: to_sql
+
+Descrição do Dado: Google BigQuery
+Tipos de Dado: SQL
+Método para Leitura: read_gbq
+Método para Escrita: to_gbq
+'''
